16b5cff9ae3132c06b4e7c594fd93e295dc1b400
malknaor <malknaor@gmail.com>
2017-08-31 20:21:36 +0300

    eGarageSystemServices.cs

---
+++ C17 Ex03 Or 200337251 Naor 301032157/Ex03.ConsoleUI/eGarageSystemServices.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ex03.ConsoleUI
+{
+    internal enum eGarageSystemServices
+    {
+        AssignVehicleToRepair = 1,
+        ViewVehiclesPlateNumbersByStatus,
+        ChangeVehicleStatus,
+        InflateVehicleWheelsToMax,
+        RefuelFuelBasedVehicle,
+        RechargeElectricBasedVehicle,
+        ViewVehicleInfo
+    }
+}
