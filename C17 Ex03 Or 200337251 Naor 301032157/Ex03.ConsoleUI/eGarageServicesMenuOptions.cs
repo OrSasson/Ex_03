41c668cd06e4880f1253765afe17389a9e75631b
malknaor <malknaor@gmail.com>
2017-09-02 21:54:13 +0300

    eGarageServicesMenuOptions

---
+++ C17 Ex03 Or 200337251 Naor 301032157/Ex03.ConsoleUI/eGarageServicesMenuOptions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ex03.ConsoleUI
+{
+    internal enum eGarageServicesMenuOptions
+    {
+        NoSelection = 0,
+        AssignVehicleToRepair,
+        ViewVehiclesPlateNumbersByStatus,
+        ChangeVehicleStatus,
+        InflateVehicleWheelsToMax,
+        RefuelFuelBasedVehicle,
+        RechargeElectricBasedVehicle,
+        ViewVehicleInfo
+    }
+}
